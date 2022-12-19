040dbda2d3ece9c16f551f35e81749fc65bb14ae
kluu0012 <kevin.lu902@gmail.com>
2022-12-19 14:12:34 +1100

    Added buffering to audio input to VOSK AI to prevent cut off
    Also added graceful termination with a 'q' key press

--- SpeechToText.py
+++ SpeechToText.py
@@ -9,10 +9,15 @@ import threading
 import queue
 import json
 import cv2
+import numpy as np
+import keyboard
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from itertools import count
 from vosk import Model, KaldiRecognizer
 
 def install_packages():
-    required = {"pyaudio", "vosk", "transformers", "torch"}
+    required = {"pyaudio", "vosk", "transformers", "torch", "keyboard"}
     # Get installed packages from pkg_resources
     installed = {pkg.key for pkg in pkg_resources.working_set}
     missing = required - installed
@@ -25,14 +30,14 @@ class MicrophoneAudio():
         self.DEVICE_INDEX = deviceindex
         self.CHANNELS = 1
         self.FRAME_RATE = 16000
-        self.RECORD_SECONDS = 2
+        self.RECORD_SECONDS = 0.5
         self.AUDIO_FORMAT = pyaudio.paInt16
         self.SAMPLE_SIZE = 2
         self.CHUNK = 1024
         self.frames = []
         self.recordings = queue.Queue()
         self.p = pyaudio.PyAudio()
-        self.stream = p.open(format=self.AUDIO_FORMAT,
+        self.stream = self.p.open(format=self.AUDIO_FORMAT,
                                 channels=self.CHANNELS,
                                 rate=self.FRAME_RATE,
                                 input=True,
@@ -67,26 +72,60 @@ class MicrophoneAudio():
 
 
 class SpeechToText():
-    def __init__(self, deviceindex):
+    def __init__(self, deviceindex, threshold=100):
         self.audio = MicrophoneAudio(deviceindex)
         self.model = Model(model_name="vosk-model-small-en-us-0.15")
         self.rec = KaldiRecognizer(self.model, self.audio.FRAME_RATE)
         self.rec.SetWords(True)
         self.quit = False
         self.SpeechToTextThread = threading.Thread(target=self.speech_recognition, args=(), name="SpeechToTextThread").start()
+        self.audioInt = []
+        self.THRESHOLD = threshold  # The audio magnitude threshold to continue to record/buffer without inputting to Vosk AI.
     
-
     def speech_recognition(self):
         while not self.quit:
             frames = self.audio.recordings.get()
-            self.rec.AcceptWaveform(b''.join(frames))
+            # convert frames to int so that we can plot them
+            audioBit = b''.join(frames)
+
+            # Buffer audio before inputting into VOSK AI to prevent cut offs
+            latestAudioBit = audioBit
+            audioMagnitude = self.THRESHOLD
+            while audioMagnitude >= self.THRESHOLD:
+                # If audio magnitude in the cycle exceeds a threshold, add more audio (another cycle) until silent
+                self.audioInt = np.frombuffer(latestAudioBit, dtype='<i2').reshape(-1, self.audio.CHANNELS)
+                audioMagnitude = np.average(np.abs(self.audioInt))
+                if audioMagnitude > self.THRESHOLD:
+                    print(f"BUFFERING AUDIO {audioMagnitude}")
+                    time.sleep(self.audio.RECORD_SECONDS)  # Wait till next recording frames
+                    # Get the frames and append them to the audio bits to input into the AI
+                    frames = self.audio.recordings.get()
+                    latestAudioBit = b''.join(frames)
+                    audioBit += latestAudioBit
+
+            # Pass audio into vosk AI
+            self.rec.AcceptWaveform(audioBit)
             result = self.rec.Result()
             text = json.loads(result)["text"]
             if text != "":
                 print(f"{text}", end=' ')
-            #time.sleep(1)
+        # Terminate SpeechToText Thread
         self.audio.close()
         print("SpeechToText was Successfully Terminated!")
+
+
+    def animate(self, i):
+        """
+        Plots the audio file to a live graph
+        """
+        y = self.audioInt[0:len(ai.audioInt):100]
+        x = np.arange(0, len(y), dtype=int)
+        plt.cla()
+        plt.plot(x, y, label='Microphone Audio')
+        plt.ylim((-1000, 1000))
+        plt.tight_layout()
+        if self.quit:
+            plt.close()
 
 
     def close(self):
@@ -95,6 +134,13 @@ class SpeechToText():
 
     def __del__(self):
         self.close()
+
+def TerminateProgramThread(ai:SpeechToText):
+    while True:
+        if keyboard.is_pressed('q'):
+            ai.close()
+            print("\n User Pressed 'q', Terminating Program!")
+            break
 
 if __name__ == "__main__":
     #install_packages()
@@ -106,7 +152,16 @@ if __name__ == "__main__":
     p.terminate()
     
     ai = SpeechToText(index)
-    print("Converting Speech To Text!")
-    input("Enter any key to quit!\n")
+
+    # Start the terminate program thread to wait for 'q' to be pressed
+    threading.Thread(target=TerminateProgramThread, args=(ai,)).start()
+
+    # Plot live Audio
+    ani = FuncAnimation(plt.gcf(), ai.animate, interval=100)
+    plt.tight_layout()
+    plt.show()
+
+    #print("Converting Speech To Text!")
+    #input("Enter any key to quit!\n")
     ai.close()
     cv2.destroyAllWindows()
